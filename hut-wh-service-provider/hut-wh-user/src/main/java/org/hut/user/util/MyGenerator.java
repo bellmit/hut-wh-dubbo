dd05a6a4050544ca8e3d7557d83d484620f078fc
hutwanghui <zjjhwanghui@163.com>
2018-11-25 09:46:43 +0800

    :egg: Adding an easter egg. 更新代码生成器

--- hut-wh-service-provider/hut-wh-user/src/main/java/org/hut/user/util/MyGenerator.java
+++ hut-wh-service-provider/hut-wh-user/src/main/java/org/hut/user/util/MyGenerator.java
@@ -15,11 +15,13 @@ import java.util.List;
  * Created by hutwanghui on 2018/11/24.
  * email:zjjhwanhui@163.com
  * qq:472860892
+ * desc：代码生成前请确保数据库中表已经存在
  */
 public class MyGenerator {
     public static void main(String[] args) {
         String outputDir = "f://tmp";
         final String viewOutputDir = outputDir + "/view/";
+
         AutoGenerator mpg = new AutoGenerator();
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
@@ -32,9 +34,14 @@ public class MyGenerator {
         gc.setBaseResultMap(true);
         // XML columList
         gc.setBaseColumnList(true);
+        /* 自定义文件命名，注意 %s 会自动填充表实体属性！ */
+        // gc.setMapperName("%sDao");
+        // gc.setXmlName("%sDao");
+        // gc.setServiceName("MP%sService");
+        // gc.setServiceImplName("%sServiceDiy");
+        // gc.setControllerName("%sAction");
         gc.setAuthor("hutwanghui");
         mpg.setGlobalConfig(gc);
-
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
         dsc.setDbType(DbType.MYSQL);
@@ -43,13 +50,31 @@ public class MyGenerator {
         dsc.setPassword("dubbo123");
         dsc.setUrl("jdbc:mysql://localhost:3306/whdubbo?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=false");
         mpg.setDataSource(dsc);
-
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
         // strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
         // strategy.setSuperControllerClass("org.hut.common.web.BaseController");
         // 表名生成策略
         strategy.setNaming(NamingStrategy.underline_to_camel);
+        // strategy.setExclude(new String[]{"test"}); // 排除生成的表
+        // 自定义实体父类
+        // strategy.setSuperEntityClass("com.baomidou.demo.TestEntity");
+        // 自定义实体，公共字段
+        // strategy.setSuperEntityColumns(new String[] { "test_id", "age" });
+        // 自定义 mapper 父类
+        // strategy.setSuperMapperClass("com.baomidou.demo.TestMapper");
+        // 自定义 service 父类
+        // strategy.setSuperServiceClass("com.baomidou.demo.TestService");
+        // 自定义 service 实现类父类
+        // strategy.setSuperServiceImplClass("com.baomidou.demo.TestServiceImpl");
+        // 自定义 controller 父类
+        // strategy.setSuperControllerClass("com.baomidou.demo.TestController");
+        // 【实体】是否生成字段常量（默认 false）
+        // public static final String ID = "test_id";
+        // strategy.setEntityColumnConstant(true);
+        // 【实体】是否为构建者模型（默认 false）
+        // public User setName(String name) {this.name = name; return this;}
+        // strategy.setEntityBuilderModel(true);
         mpg.setStrategy(strategy);
 
         // 包配置
@@ -69,16 +94,28 @@ public class MyGenerator {
         if (!viewDir.exists()) {
             viewDir.mkdirs();
         }
-//        List<FileOutConfig> focList = new ArrayList<FileOutConfig>();
-//        focList.add(new FileOutConfig("/templates/listvue.vue.vm") {
-//            @Override
-//            public String outputFile(TableInfo tableInfo) {
-//                return getGeneratorViewPath(viewOutputDir, tableInfo, ".vue");
-//            }
-//        });
-//        cfg.setFileOutConfigList(focList);
+        //使用自定义的模板生成前端页面
+        // List<FileOutConfig> focList = new ArrayList<FileOutConfig>();
+        // focList.add(new FileOutConfig("/templates/listvue.vue.vm") {
+        // @Override
+        // public String outputFile(TableInfo tableInfo) {
+        //    return getGeneratorViewPath(viewOutputDir, tableInfo, ".vue");
+        //  }
+        //  });
+        // cfg.setFileOutConfigList(focList);
         mpg.setCfg(cfg);
-        //生成controller相关
+        // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/templates 下面内容修改，
+        // 放置自己项目的 src/main/resources/templates 目录下, 默认名称一下可以不配置，也可以自定义模板名称
+        // TemplateConfig tc = new TemplateConfig();
+        // tc.setController("...");
+        // tc.setEntity("...");
+        // tc.setMapper("...");
+        // tc.setXml("...");
+        // tc.setService("...");
+        // tc.setServiceImpl("...");
+        // 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
+        // mpg.setTemplate(tc);
+        //执行
         mpg.execute();
     }
 }
